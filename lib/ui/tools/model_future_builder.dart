591e2fa7aea4ab0e7daff802236afbec70c7c3e2
ajmal-km <ajmalkm125@gmail.com>
2024-10-24 14:59:56 +0530

    edited

--- lib/ui/tools/model_future_builder.dart
+++ lib/ui/tools/model_future_builder.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../constants/app_colors.dart';
 import '../widgets/shared.dart';
 
 class ModelFutureBuilder<T> extends StatelessWidget {
